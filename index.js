302b1d65246786d680cc71b944e57a326a53efa6
Nicholas Ndikwe <nicholasndikwe@gmail.com>
2022-08-31 16:06:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const submitData = {
+    userName: "Nicholas",
+    userEmail: "Ndikwe",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("not good");
+      console.log(error.message);
+    });
